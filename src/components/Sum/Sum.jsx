d2870e5f0224d3f772b1282ffc02945003a790b2
torry455 <kucerv8@gmail.com>
2025-05-13 15:35:50 +0300

    added resolve problem due to AI comments

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,11 @@
-export const Sum = ({ a = 0, b = 0, sum = a + b }) => (
-  <div>
-    <p>
-      Sum of {a} and {b} is {sum}
-    </p>
-  </div>
-);
+export const Sum = ({ a = 0, b = 0 }) => {
+  const sum = a + b;
+
+  return (
+    <div>
+      <p>
+        Sum of {a} and {b} is {sum}
+      </p>
+    </div>
+  );
+};
